1550e0d139a4a6724928be63efa4a5fdebc50a5e
Rehan Adi Satrya <47605698+rehanadi30@users.noreply.github.com>
2021-02-24 00:21:38 +0700

    Polygon Aman

---
+++ poligon.js
@@ -0,0 +1,29 @@
+function clearPoligon(){
+    if(coorPoly.length!==0){
+        coorPoly=[];
+    }
+}
+
+function makePoligon(polygonVertices, numOfVertices) {
+    var polygonBuffer = gl.createBuffer();
+    // Binding buffernya
+    gl.bindBuffer(gl.ARRAY_BUFFER, polygonBuffer);
+    // Isi data
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(polygonVertices), gl.STATIC_DRAW);
+
+    //Ambil koordinat titik dan angka
+    var loc = gl.getAttribLocation(program, "vPosition");
+    var colLoc = gl.getAttribLocation(program, "vertcolor");
+
+    // Masukkan rule atribut
+    gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.vertexAttribPointer(loc, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+
+    // Masukkan atribut
+    gl.enableVertexAttribArray(loc);
+    gl.enableVertexAttribArray(colLoc);
+
+    // Render poligonnya
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, numOfVertices);
+}
