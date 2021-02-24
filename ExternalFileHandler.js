f19d8dc0b68cca3c979eedb9421fd74d443aadbc
Rehan Adi Satrya <47605698+rehanadi30@users.noreply.github.com>
2021-02-24 21:42:55 +0700

    Load file selesai tapi belum bisa render

--- ExternalFileHandler.js
+++ ExternalFileHandler.js
@@ -1,9 +1,9 @@
 var saveFile = document.getElementById("saveBtn");
 saveFile.addEventListener("click", function() {
-    // Create text document — only saves 1st link in text doc
+    // Membuat document baru berupa text
     var textDoc = document.createElement('a');
     var stringFinal = '';
-    // var stringNtar = 'data:attachment/text,' + encodeURI(coorPoly.join('\n')) + '\ngaris\n' + encodeURI(coorPoly.join('\n')) + '\nkotak' + encodeURI(coorPoly.join('\n')) + '\npolygon');
+    
     if(coorGaris.length != 0){
         stringFinal += encodeURI(coorGaris.join('\n')) + '\ngaris\n';
     }
@@ -30,17 +30,42 @@ loadFile.addEventListener("change", function() {
         for (var line = 0; line < lines.length; line++){
             if(lines[line] === "kotak" || lines[line] === "garis" || lines[line] === "polygon"){
                 console.log("masuk shape");
-                if(lines[line] === "kotak"){
-                    console.log("masuk kotak");
+                if(lines[line] === "garis"){
+                    console.log("ada garis");
+                    for (i = 0; i<lines.length; i++){
+                        if(lines[i] === "garis"){
+                            break;
+                        }
+                        coorGaris = lines[i];
+                    }
+                    makeGaris(coorGaris, coorGaris.length/5);
                 }
-                else if(lines[line] === "garis"){
-                    console.log("masuk garis");
+                else if(lines[line] === "kotak"){
+                    console.log("ada kotak");
+                    for (i = 0; i<lines.length; i++){
+                        if(lines[i] === "garis"){
+                            coorSquare = []; //kalo ketemu garis, kosongin dulu
+                        }
+                        if(lines[i] === "kotak"){
+                            break;
+                        }
+                        coorSquare = lines[i];
+                    }
+                    makePersegi(coorSquare, coorSquare.length/5);
                 }
                 else if(lines[line] === "polygon"){
                     console.log("masuk polygon");
-                    for (i = 0; i < 15; i++){
-                        coorPoly.push(parseFloat(lines[i]))
-                        console.log(coorPoly);
+                    for (i = 0; i<lines.length; i++){
+                        if(lines[i] === "garis"){
+                            coorSquare = []; //kalo ketemu garis, kosongin dulu
+                        }
+                        if(lines[i] === "kotak"){
+                            coorSquare = [];
+                        }
+                        if(lines[i] === "polygon"){
+                            break;
+                        }
+                        coorSquare = lines[i];
                     }
                     // console.log(coorPoly);
                     // editMode = 1;
